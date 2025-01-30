ecd0a670febfdf21a0434fb991e538dc55beffd1
Dom <Dom@DESKTOP-8705DDO>
2025-01-30 16:14:47 +0500

    Добавьте файлы проекта.

---
+++ MauiAppTodolist/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿
+using System.Collections.ObjectModel;
+
+namespace MauiAppTodolist
+
+{
+    public partial class MainPage : ContentPage
+    {
+        private ObservableCollection<string> tasks;
+
+
+
+        public MainPage()
+        {
+            InitializeComponent();
+            tasks = new ObservableCollection<string>();
+            TasksListView.ItemsSource = tasks;
+        }
+
+        private void OnAddTaskClicked(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(TaskEntry.Text))
+            {
+                tasks.Add(TaskEntry.Text);
+                TaskEntry.Text = string.Empty; 
+            }
+        }
+
+        private void OnDeleteTaskClicked(object sender, EventArgs e)
+        {
+            var button = sender as Button;
+            var task = button.CommandParameter as string;
+            tasks.Remove(task); 
+        }
+
+    }
+
+}
